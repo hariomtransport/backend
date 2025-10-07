e34d6c30f8413ded8c53cee2970cdcad4bdb5309
mehul-1009 <mehul.parmar@entanglesoftware.com>
2025-10-07 16:15:54 +0530

    minor fix for mobile

--- models/initial.go
+++ models/initial.go
@@ -2,15 +2,20 @@ package models
 
 import "time"
 
+type MobileEntry struct {
+	Number string `json:"number" bson:"number" db:"number"`
+	Label  string `json:"label" bson:"label" db:"label"`
+}
+
 type InitialSetup struct {
-	ID          int64     `json:"id" bson:"_id,omitempty" db:"id"`
-	CompanyName string    `json:"company_name" bson:"name" db:"name"`
-	Address     string    `json:"address" bson:"address" db:"address"`
-	City        string    `json:"city" bson:"city" db:"city"`
-	State       string    `json:"state" bson:"state" db:"state"`
-	Pincode     string    `json:"pincode" bson:"pincode" db:"pincode"`
-	GSTIN       string    `json:"gstin" bson:"gstin" db:"gstin"`
-	Footnote    string    `json:"footnote" bson:"footnote" db:"footnote"`
-	Mobile      []string  `json:"mobile" bson:"mobile" db:"mobile"` // New mobile numbers field
-	CreatedAt   time.Time `json:"created_at" bson:"created_at" db:"created_at"`
+	ID          int64         `json:"id" bson:"_id,omitempty" db:"id"`
+	CompanyName string        `json:"company_name" bson:"name" db:"name"`
+	Address     string        `json:"address" bson:"address" db:"address"`
+	City        string        `json:"city" bson:"city" db:"city"`
+	State       string        `json:"state" bson:"state" db:"state"`
+	Pincode     string        `json:"pincode" bson:"pincode" db:"pincode"`
+	GSTIN       string        `json:"gstin" bson:"gstin" db:"gstin"`
+	Footnote    string        `json:"footnote" bson:"footnote" db:"footnote"`
+	Mobile      []MobileEntry `json:"mobile" bson:"mobile" db:"mobile"`
+	CreatedAt   time.Time     `json:"created_at" bson:"created_at" db:"created_at"`
 }
